be506bc2fb389715782dfa99b2db030c63203c3b
Devanshu Jain <jaindevanshu236@gmail.com>
2024-06-14 23:36:27 +0530

    git_ignore

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,14 @@ const express = require('express');
 const app = express();
 require('./db/conn');
 const users = require('./models/userSchema');
+const cors = require('cors');
 
 const port = 8081;
 
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.send("Hello Server!!");
